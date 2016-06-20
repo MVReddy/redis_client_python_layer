c4cd5a0fd20dd08dbfd08e4afb3873d36b357d38
Venkata Reddy M <venkatareddy.mulam@techmahindra.com>
2016-06-20 17:20:37 -0400

    Fixes generic cache implementation

--- generic_cache.py
+++ generic_cache.py
@@ -8,7 +8,6 @@ from functools import wraps
 import json
 import hashlib
 import redis
-from redis.exceptions import ConnectionError, RedisError
 import logging
 import time
 import time as timer
@@ -16,7 +15,7 @@ try:
     import cPickle as pickle
 except:
     import pickle
-from mass_redis_cache.settings import get_settings
+from settings import get_settings
 
 DEFAULT_EXPIRY = 60 * 60 * 24
 env_settings = get_settings()
@@ -26,7 +25,25 @@ REDIS_PASSWORD = env_settings.REDIS_PASSWORD
 REDIS_DB = env_settings.REDIS_DB
 
 
-class MIOCache(redis.client.Redis):
+# create the cache key for storage
+def cache_create_key(namespace, ignore_args, func_name, *args, **kwargs):
+    import hashlib
+    serialise = []
+    if ignore_args:
+        serialise.append(str(args[0]))
+    else:
+        for arg in args:
+            serialise.append(str(arg))
+        for key, arg in kwargs.items():
+            serialise.append(str(key))
+            serialise.append(str(arg))
+    key = hashlib.md5("".join(serialise)).hexdigest()
+    if namespace:
+        key = ':'.join([namespace, func_name, key])
+    return key
+
+
+class MyCache(redis.client.Redis):
     """
     Generic redis Cache class which uses the internal redis-py cliet 
     Provides genaric implementation of redis cache using Python client
@@ -48,22 +65,27 @@ class MIOCache(redis.client.Redis):
         else:
             self.max_sleep = 30
 
-        if 'logger' in kwargs:
-            self.logger = kwargs.pop('logger')
+        if 'log' in kwargs:
+            self._log = kwargs.pop('log')
         else:
-            self.logger = logging
+            self._log = logging.getLogger(env_settings.LOG_NAME)
 
         if 'prefix' in kwargs:
             self.prefix = kwargs.pop('prefix')
         else:
             self.prefix = None
 
+        if 'hashkeys' in kwargs:
+            self.prefix = kwargs.pop('hashkeys')
+        else:
+            self.hashkeys = True
+
         self.host = kwargs.get('host', REDIS_HOST)
         self.port = kwargs.get('port', REDIS_PORT)
         self.db = kwargs.get('db', REDIS_DB)
         self.password = kwargs.get('password', REDIS_PASSWORD)
 
-        self.connection = self.get_connection()
+        self.connection = self.connect()
 
     def ping(self):
         """
@@ -71,9 +93,11 @@ class MIOCache(redis.client.Redis):
         :return boolean value True if redis connection is alive else False
         """
         try:
-            super(MIOCache, self).ping()
+            self.connection.ping()
+            self._log.info("Able to ping Redis Server")
             return True
-        except:
+        except Exception as e:
+            self._log.info("Unable to ping Redis Server: %s" % e)
             return False
 
     def connect(self, *args, **kwargs):
@@ -89,12 +113,12 @@ class MIOCache(redis.client.Redis):
         try:
             connection = redis.StrictRedis(host=host, port=port, db=db, password=password)
             connection.ping()
-            self.logger.info("Successfully connected to redis with: ", host, port)
+            self._log.info("Successfully connected to redis with: %s, %s" % (host, port))
             self.connection = connection
             return connection
         except redis.ConnectionError as e:
-            self.logger.error("Failed to create connection to redis with: ", host, port)
-            self.logger.error("Please check the redis connection. ERROR: ", e)
+            self._log.error("Failed to create connection to redis with: %s, %s" % (host, port))
+            self._log.error("Please check the redis connection. ERROR: %s" % e)
 
     def reconnect(self, conn_retries=None):
         """ Connects to Redis with a exponential waiting (3**n) """
@@ -102,19 +126,19 @@ class MIOCache(redis.client.Redis):
             conn_retries = self.conn_retries
 
         count = 0
-        if self.logger:
-            self.logger.info('Connecting to Redis.')
+        if self._log:
+            self._log.info('Connecting to Redis.')
         while count < conn_retries:
             super(redis.client.Redis, self).__init__(*self.args, **self.kwargs)
 
             if self.ping():
-                if self.logger:
-                    self.logger.info('Connected to Redis!')
+                if self._log:
+                    self._log.info('Connected to Redis!')
                 return True
             else:
                 sl = min(3 ** count, self.max_sleep)
-                if self.logger:
-                    self.logger.info('Connecting failed, retrying in {0} seconds'.format(sl))
+                if self._log:
+                    self._log.info('Connecting failed, retrying in {0} seconds'.format(sl))
                 time.sleep(sl)
                 count += 1
         return False
@@ -122,29 +146,32 @@ class MIOCache(redis.client.Redis):
     def safe_reconnect(self):
         """ Connects to Redis with a exponential waiting (3**n), wont return until successfully connected"""
         count = 0
-        if self.logger:
-            self.logger.info('Connecting to Redis.')
+        if self._log:
+            self._log.info('Connecting to Redis.')
         while True:
             super(redis.client.Redis, self).__init__(*self.args, **self.kwargs)
 
             if self.ping():
-                if self.logger:
-                    self.logger.info('Connected to Redis!')
+                if self._log:
+                    self._log.info('Connected to Redis!')
                 return True
             else:
                 sl = min(3 ** count, self.max_sleep)
-                if self.logger:
-                    self.logger.info('Connecting failed, retrying in {0} seconds'.format(sl))
+                if self._log:
+                    self._log.info('Connecting failed, retrying in {0} seconds'.format(sl))
                 time.sleep(sl)
                 count += 1
 
     def make_key(self, key):
-        return "{0}:{1}".format(self.prefix, key)
+        if self.prefix:
+            return "{0}:{1}".format(self.prefix, key)
+
+        return key
 
     def namespace_key(self, namespace):
         return namespace + ':*'
 
-    def get_key(self, key):
+    def get(self, key):
         """
         Method returns value of a given key from the cache else None.
         :param keys: List of keys to look up in Redis
@@ -153,15 +180,15 @@ class MIOCache(redis.client.Redis):
         key = to_unicode(key)
         if key:  # No need to validate membership, which is an O(1) operation, but seems we can do without.
             start = timer.time()
-            value = self.get(self.make_key(key))
+            value = self.connection.get(self.make_key(key))
             if not value:  # expired key
-                self.logger.info('Key - %s Not found' % key)
+                self._log.info('Key - %s Not found' % key)
                 return
 
-            self.logger("Cache took {0} to retrieve data from the Redis Server".format(float(timer.time() - start)))
+            self._log.info("Cache took {0} to retrieve data from the Redis Server".format(float(timer.time() - start)))
             return pickle.loads(value)
 
-    def get_keys(self, keys):
+    def mget(self, keys):
         """
         Method returns a dict of key/values for found keys.
         :param keys: List of keys to look up in Redis
@@ -169,9 +196,18 @@ class MIOCache(redis.client.Redis):
         """
         if keys:
             cache_keys = [self.make_key(to_unicode(key)) for key in keys]
-            values = self.mget(cache_keys)
+            values = self.connection.mget(cache_keys)
 
             return {k: pickle.loads(v) for (k, v) in zip(keys, values)}
+
+    def keys(self):
+        """
+        Returns all keys in the connection in a list
+        """
+        start = timer.time()
+        keys = self.connection.keys()
+        self._log.info("Cache took {0} to retrieve data from the Redis Server".format(float(timer.time() - start)))
+        return keys
 
     def get_keys_json(self, keys):
         """
@@ -180,13 +216,15 @@ class MIOCache(redis.client.Redis):
         :param keys: List of keys to look up in Redis
         :return: dict of found key/values with values parsed from JSON format
         """
-        d = self.mget(keys)
+        start = timer.time()
+        d = self.connection.mget(keys)
+        self._log.info("Cache took {0} to retrieve data from the Redis Server".format(float(timer.time() - start)))
         if d:
             for key, value in d.items():
                 d[key] = json.loads(value if value else None)
             return d
 
-    def store(self, key, value, expire=DEFAULT_EXPIRY):
+    def set(self, key, value, expire=DEFAULT_EXPIRY):
         """
         Method stores a value in the cache with expiration
         :param key: key by which to reference datum being stored in Redis
@@ -199,31 +237,35 @@ class MIOCache(redis.client.Redis):
         if expire is None:
             expire = self.expire
 
-        self.set(self.make_key(key), value, expire)
+        self.connection.set(self.make_key(key), value, expire)
+        self._log.info("Successfully set %s: %s" % (key, value))
 
-    def flush_key(self, key):
+    def delete(self, key):
         """
         Method removes (invalidates) an item from the cache.
         :param key: key to remove from Redis
         """
         key = to_unicode(key)
-        self.delete(self.make_key(key))
+        self.connection.delete(self.make_key(key))
+        self._log.info("Successfully deleted key: %s" % key)
 
-    def flush_all(self):
+    def delete_all(self):
         """
         Method removes (invalidates) all items from the cache.
         """
-        keys = self.keys()
-        self.delete(*keys)
+        keys = self.connection.keys()
+        self.connection.delete(*keys)
+        self._log.info("Successfully deleted keys: %s" % keys)
 
-    def flush_namespace(self, space):
+    def delete_namespace(self, space):
         """
         Method removes (invalidates) all items in a given namespace from the cache.
         :param key: key to remove from Redis
         """
         namespace = self.namespace_key(space)
-        keys = list(self.keys(namespace))
-        self.delete(*keys)
+        keys = list(self.connection.keys(namespace))
+        self.connection.delete(*keys)
+        self._log.info("Successfully deleted namespace: %s" % namespace)
 
     def isexpired(self, key):
         """
@@ -234,7 +276,7 @@ class MIOCache(redis.client.Redis):
         """
         ttl = self.pttl(key)
         if ttl == -2:  # not exist
-            ttl = self.pttl(self.make_key(key))
+            ttl = self.connection.pttl(self.make_key(key))
         elif ttl == -1:
             return True
         if not ttl:
@@ -243,19 +285,19 @@ class MIOCache(redis.client.Redis):
             return self.pttl("{0}:{1}".format(self.prefix, key))
 
     def store_json(self, key, value, expire=None):
-        self.set(key, json.dumps(value), expire)
+        self.connection.set(key, json.dumps(value), expire)
 
     def store_pickle(self, key, value, expire=None):
-        self.set(key, pickle.dumps(value), expire)
+        self.connection.set(key, pickle.dumps(value), expire)
 
     def get_json(self, key):
-        return json.loads(self.get(key))
+        return json.loads(self.get_key(key))
 
     def get_pickle(self, key):
-        return pickle.loads(self.get(key))
+        return pickle.loads(self.get_key(key))
 
     def __contains__(self, key):
-        return key in self.keys()
+        return key in self.connection.keys()
 
     def get_hash(self, args):
         if self.hashkeys:
@@ -265,11 +307,9 @@ class MIOCache(redis.client.Redis):
         return key
 
 
-def cache_it(limit=10000, expire=DEFAULT_EXPIRY, cache=None,
-             use_json=False, namespace=None, view=True):
+def cache_it(namespace=None, expire=DEFAULT_EXPIRY, cache=None, ignore_args=False, use_json=False, view=False):
     """
     Arguments and function result must be pickleable.
-    :param limit: maximum number of keys to maintain in the set
     :param expire: period after which an entry in cache is considered expired
     :param cache: SimpleCache object, if created separately
     :return: decorated function
@@ -280,43 +320,34 @@ def cache_it(limit=10000, expire=DEFAULT_EXPIRY, cache=None,
     def decorator(function):
         cache, expire = cache_, expire_
         if cache is None:
-            if namespace:
-                cache = MIOCache(limit, expire, hashkeys=True, namespace=namespace)
-            else:
-                cache = MIOCache(limit, expire, hashkeys=True)
-        elif expire == DEFAULT_EXPIRY:
-            # If the expire arg value is the default, set it to None so we set
-            # the expire value of the passed cache object
-            expire = None
+            cache = MIOCache(namespace=namespace)
 
         @wraps(function)
         def func(*args, **kwargs):
             # Handle cases where caching is down or otherwise not available.
-            if cache.connection is None:
+            if cache.connection is None or cache.ping() is False:
                 result = function(*args, **kwargs)
                 return result
 
-            serializer = json if use_json else pickle
+            # serializer = json if use_json else pickle
             fetcher = cache.get_json if use_json else cache.get_pickle
             # This way, you need to make sure all args must be json or pcikle serializable.
             storer = cache.store_json if use_json else cache.store_pickle
 
-            # Key will be either a md5 hash or just pickle object,
-            # in the form of `function name`:`key`
-            key = cache.get_hash(serializer.dumps([args, kwargs]))
-            cache_key = '{func_name}:{key}'.format(func_name=function.__name__, key=key)
+            cache_key = cache_create_key(namespace, ignore_args, func.__name__, *args, **kwargs)
 
             try:
                 start = timer.time()
                 result = fetcher(cache_key)
-                print "Cache took {0} to retireve data from the Redis Server".format(float(timer.time() - start))
+                msg = "Cache took {0} to retireve data from the Redis Server".format(float(timer.time() - start))
+                cache._log.info(msg)
                 return result
 
             except Exception as e:
                 # Add some sort of cache miss handing here.
                 pass
             except:
-                logging.exception("Unknown redis-cache error. Please check your Redis free space.")
+                cache._log.info("Unknown redis-cache error. Please check your Redis free space.")
 
             try:
                 result = function(*args, **kwargs)
@@ -326,7 +357,8 @@ def cache_it(limit=10000, expire=DEFAULT_EXPIRY, cache=None,
                 try:
                     start = timer.time()
                     storer(cache_key, result, expire)
-                    print "Cache took {0} to set data from the Redis Server".format(float(timer.time() - start))
+                    msg = "Cache took {0} to retireve data from the Redis Server".format(float(timer.time() - start))
+                    cache._log.info(msg)
                 except redis.ConnectionError as e:
                     logging.exception(e)
 
@@ -335,7 +367,7 @@ def cache_it(limit=10000, expire=DEFAULT_EXPIRY, cache=None,
     return decorator
 
 
-def cache_it_json(limit=10000, expire=DEFAULT_EXPIRY, cache=None, namespace=None, view=True):
+def cache_it_json(expire=DEFAULT_EXPIRY, cache=None, namespace=None, view=True):
     """
     Arguments and function result must be able to convert to JSON.
     :param limit: maximum number of keys to maintain in the set
@@ -343,8 +375,7 @@ def cache_it_json(limit=10000, expire=DEFAULT_EXPIRY, cache=None, namespace=None
     :param cache: SimpleCache object, if created separately
     :return: decorated function
     """
-    return cache_it(limit=limit, expire=expire, use_json=True,
-                    cache=cache, namespace=None)
+    return cache_it(expire=expire, use_json=True, cache=cache, namespace=None)
 
 
 def to_unicode(obj, encoding='utf-8'):
@@ -352,3 +383,4 @@ def to_unicode(obj, encoding='utf-8'):
         if not isinstance(obj, unicode):
             obj = unicode(obj, encoding)
     return obj
+
